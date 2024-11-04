ab97056e6bffd1ddde733237e0a31501a0b1f7bf
Rachit Garg <rachitgarg900@gmail.com>
2024-11-04 08:53:26 +0100

    Update 08_Lambdas.kt with implicit param

--- 01_Introduction/08_Lambdas.kt
+++ 01_Introduction/08_Lambdas.kt
@@ -1,3 +1,2 @@
 fun containsEven(collection: Collection<Int>): Boolean =
-        collection.any {x -> val hasEven = x % 2 == 0;
-            hasEven}
+        collection.any { it % 2 == 0 } // Implicit param should be named 'it'
