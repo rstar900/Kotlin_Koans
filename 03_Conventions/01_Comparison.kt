f00a18a5beee07baf7238bf0ee3ee7ac460b5b71
Rachit Garg <rachitgarg900@gmail.com>
2024-11-09 14:40:52 +0100

    Update 01_Comparison.kt
    Added an easier solution for 01_Comparison.kt

--- 03_Conventions/01_Comparison.kt
+++ 03_Conventions/01_Comparison.kt
@@ -2,17 +2,11 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     // Adding compareTo() operator overload will make it comparable class
     override operator fun compareTo(other: MyDate): Int {
         // Compare in order of year, month and then day (if anything unequal return a value)
-        val result =  when (val yrRes = year.compareTo(other.year)) {
-            0 -> when (val monRes = month.compareTo(other.month)) {
-                0 -> when (val dayRes = dayOfMonth.compareTo(other.dayOfMonth)) {
-                    0 -> 0
-                    else -> dayRes
-                }
-                else -> monRes
-            }
-            else -> yrRes
+        return when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
         }
-        return result
     }
 }
 
