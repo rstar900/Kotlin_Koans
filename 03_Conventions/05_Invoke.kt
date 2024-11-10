0a16c355d4597e8bec05db9ca4690328ae59fa2a
Rachit Garg <rachitgarg900@gmail.com>
2024-11-10 23:21:58 +0100

    Update 05_Invoke.kt

--- 03_Conventions/05_Invoke.kt
+++ 03_Conventions/05_Invoke.kt
@@ -1,1 +1,12 @@
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
 
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
